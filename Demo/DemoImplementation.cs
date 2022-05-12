f7c56e469b342905ba227244a7eab47aedf9a274
Rolczak <kc.rolczak@gmail.com>
2022-05-12 21:44:26 +0200

    Add project files.

---
+++ Demo/DemoImplementation.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoImplementation
+{
+    public static class DemoImplementation
+    {
+        public static IEnumerable<DemoTarget.PersonWithEmail> Flatten(IEnumerable<DemoSource.Person> people)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Demo/DemoSource.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoSource
+{
+    public class Person
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public IEnumerable<EmailAdress> Emails { get; set; } = Enumerable.Empty<EmailAdress>();
+    }
+
+    public class EmailAdress
+    {
+        public string Email { get; set; } = null!;
+        public string EmailType { get; set; } = null!;
+    }
+}
