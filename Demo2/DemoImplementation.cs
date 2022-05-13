fbf86af62f08f11d0da356a798fb9af4e9a4b413
Rolczak <kc.rolczak@gmail.com>
2022-05-13 21:26:45 +0200

    Add Task2 project

---
+++ Demo2/DemoImplementation.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DemoImplementation
+{
+    public static class ToDo
+    {
+        public static IEnumerable<(DemoSource.Account, DemoSource.Person)> MatchPersonToAccount(IEnumerable<DemoSource.Group> groups,
+                                                                                        IEnumerable<DemoSource.Account> accounts,
+                                                                                        IEnumerable<string> emails)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Demo2/DemoSource.cs
@@ -0,0 +1,28 @@
+﻿namespace DemoSource
+{
+    public class Person
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public IEnumerable<EmailAdress> Emails { get; set; } = Enumerable.Empty<EmailAdress>();
+    }
+
+    public class EmailAdress
+    {
+        public string Email { get; set; } = null!;
+        public string EmailType { get; set; } = null!;
+    }
+
+    public class Account
+    {
+        public string Id { get; set; } = null!;
+        public EmailAdress EmailAdress { get; set; } = null!;
+    }
+
+    public class Group
+    {
+        public string Id { get; set; } = null!;
+        public string Label { get; set; } = null!;
+        public IEnumerable<Person> People { get; set; } = Enumerable.Empty<Person>();
+    }
+}
