79ce177b46d540050e56204f8ac4bfca2afc5c51
Rolczak <kc.rolczak@gmail.com>
2022-05-14 02:09:06 +0200

    Optimize for searching large sets

--- Demo2/DemoImplementation.cs
+++ Demo2/DemoImplementation.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Concurrent;
-using System.Text.RegularExpressions;
 
 namespace DemoImplementation
 {
@@ -11,10 +10,21 @@ namespace DemoImplementation
         {
             var accountsAndPersons = new ConcurrentBag<(DemoSource.Account, DemoSource.Person)>();
 
+            //Flattening data helps searching performance with little bit cost of memory
+            //I assumend that we will be matching many emails (I don't know why)
+            //For matching small ammounts of emails previous version is little faster (Previous commit)
+            //but when matchin more emails ex. 16k flattening structure will benefit a lot
+            //Example with 16k emails and very unfortunate emails placement (tests with exactly same data)
+            //without flattening: Matching completed in: 00:00:45.2234191 Memory: 119MB
+            //with flattening: Matching completed in: 00:00:23.0623495 Memory: 130MB
+
+            var peopleWithEmail = groups.AsParallel().SelectMany(x => x.People).SelectMany(p => p.Emails.Select(e => new { Person = p, Email = e })).ToList();
+
             Parallel.ForEach(emails, email =>
             {
                 var account = accounts.Where(a => a.EmailAdress.Email == email).First();
-                var person = groups.SelectMany(x => x.People).Where(x => x.Emails.Any(x => x == account.EmailAdress)).First();
+                //Assuming that EmailAdress has same reference in Person and Account
+                var person = peopleWithEmail.Where(pe => pe.Email == account.EmailAdress).Select(pe => pe.Person).First();
                 accountsAndPersons.Add((account, person));
             });
 
